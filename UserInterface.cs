7a7fb97e64d63f56ea78bf9a5834bb70793a6679
Kyle Melton <87396239+KyleMel@users.noreply.github.com>
2021-09-14 15:23:06 -0500

    create methods in UI class for output and an input check

--- UserInterface.cs
+++ UserInterface.cs
@@ -9,6 +9,21 @@ namespace HangmanGame
     // WriteLine, ReadLine Methods for program
     public class UserInterface
     {
+        public void Write(string toCon)
+        {
+            Console.WriteLine(toCon);
+        }
+        public char Input(string ask)
+        {
+            Console.Write(ask);
+            var response = Console.ReadLine().ToUpper();
+            char myChar;
 
+            if (!Char.TryParse(response, out myChar))
+            {
+                Input(ask);
+            }
+            return myChar;
+        }
     }
 }
